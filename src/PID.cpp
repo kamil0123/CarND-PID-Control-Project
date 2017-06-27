0a2aa3ef4fca8552fe3fc4f9ff487e5d34f00eb0
Kamil <kamil.kindziuk@gmail.com>
2017-06-27 18:52:54 +0200

    Add PID initialization in main.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,14 +11,14 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
-	
-	p_error = 0.0;
-	d_error = 0.0;
-	i_error = 0.0;
-	
-	PID::Kp = Kp;
-    PID::Ki = Ki;
-    PID::Kd = Kd;
+
+  p_error = 0.0;
+  d_error = 0.0;
+  i_error = 0.0;
+
+  PID::Kp = Kp;
+  PID::Ki = Ki;
+  PID::Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
