535964a40eacfeb928ab28588c727a64329a7093
Kamil <kamil.kindziuk@gmail.com>
2017-06-28 07:09:51 +0200

    update updateError and TotalError funcions

--- src/PID.cpp
+++ src/PID.cpp
@@ -22,8 +22,13 @@ void PID::Init(double Kp, double Ki, double Kd) {
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error; 
+  p_error = cte;
+  i_error += cte;
 }
 
 double PID::TotalError() {
+  double totalError = - Kp*p_error - Ki*i_error - Kd*d_error;
+  return totalError;
 }
 
